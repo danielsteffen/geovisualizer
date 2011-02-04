09b2da325ff3d36ed5b9375d02f11ce14150519b
Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>
2011-02-04 09:52:37 +0100

    Exception Handling & Logging of exceutable Main Class.
    	*Catches an Exception if something goes wrong during the startup of the application.
    	*Logging.
    
    
    Signed-off-by: Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>

--- src/main/java/com/dfki/av/sudplan/main/Starter.java
+++ src/main/java/com/dfki/av/sudplan/main/Starter.java
@@ -2,13 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.dfki.av.sudplan.main;
 
 import com.dfki.av.sudplan.conf.ApplicationConfiguration;
 import com.dfki.av.sudplan.conf.ComponentFactory;
-import java.util.ResourceBundle;
+import com.dfki.av.sudplan.conf.InitialisationException;
 import javax.swing.SwingUtilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -17,16 +18,22 @@ import javax.swing.SwingUtilities;
  * @since 1.6
  */
 public class Starter {
-
+private final static Logger logger = LoggerFactory.getLogger(Starter.class);
+  public static void main(String[] args) {
     
-    public static void main(String[] args) {
-       
-        //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: CLI support
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ComponentFactory.getStandaloneApplication(new ApplicationConfiguration()).setVisible(true);
-            }   
-        });
-    }
+    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: CLI support
+    SwingUtilities.invokeLater(new Runnable() {
+
+      @Override
+      public void run() {
+        try {
+          ComponentFactory.getStandaloneApplication(new ApplicationConfiguration()).setVisible(true);
+        } catch (InitialisationException ex) {
+          if(logger.isErrorEnabled()){
+            logger.error("Error while starting application.",ex);
+          }
+        }
+      }
+    });
+  }
 }
