59d30dcd0861f0f09c99c7d01606155d082b5dd8
Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>
2011-02-08 09:34:18 +0100

    Minor modifications. Getting/setting, formatting.
    Signed-off-by: Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>

--- src/main/java/com/dfki/av/sudplan/conf/ComponentFactory.java
+++ src/main/java/com/dfki/av/sudplan/conf/ComponentFactory.java
@@ -4,6 +4,7 @@
  */
 package com.dfki.av.sudplan.conf;
 
+import com.dfki.av.sudplan.control.ComponentBroker;
 import com.dfki.av.sudplan.control.ComponentController;
 import com.dfki.av.sudplan.ui.MainFrame;
 import java.util.Properties;
@@ -22,35 +23,42 @@ import org.slf4j.LoggerFactory;
  */
 public class ComponentFactory {
 
-  private final static Logger logger = LoggerFactory.getLogger(ComponentFactory.class);
+    private final static Logger logger = LoggerFactory.getLogger(ComponentFactory.class);
 
-  /* ToDo would it be a good idea to search for common properties and build a abstract interface
-   * in order to build a common interface for StandaloneApplication & VisualisationPanel
-   */
-  public static JFrame getStandaloneApplication(ApplicationConfiguration configuration) throws InitialisationException {
-    if (configuration == null) {
-      if(logger.isInfoEnabled()){
-        logger.info("No configuration properties, starting with default properties.");
-      }      
-      configuration = new ApplicationConfiguration();
-    }
-    final MainFrame newApplicatioFrame = new MainFrame();    
-    ComponentController compControl;
-    try {
-      compControl = new ComponentController(newApplicatioFrame.getVisualisationComponent(), configuration);
-      newApplicatioFrame.setController(compControl);
-    } catch (TooManyListenersException ex) {
-      if(logger.isErrorEnabled()){
-        final String message = "Error during initialisation of ComponentController.";
-        logger.error(message,ex);
-        throw new InitialisationException(message,ex);
-      }
-    }
-    return newApplicatioFrame;
-  }
+    /* ToDo would it be a good idea to search for common properties and build a abstract interface
+     * in order to build a common interface for StandaloneApplication & VisualisationPanel
+     */
+    public static JFrame getStandaloneApplication(ApplicationConfiguration configuration)
+            throws InitialisationException {
+        if (configuration == null) {
+            if (logger.isInfoEnabled()) {
+                logger.info("No configuration properties, starting with default properties.");
+            }
+            configuration = new ApplicationConfiguration();
+        }
 
-  public static JPanel getVisualisationPanel(Properties configurationProperties) {
-    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: implemenent SingleMode
-    throw new UnsupportedOperationException("Not yet implemented.");
-  }
+        MainFrame newApplicatioFrame = null;
+        ComponentController compControl = null;
+        try {
+            newApplicatioFrame = new MainFrame();
+            ComponentBroker.getInstance().setMainFrame(newApplicatioFrame);
+            compControl = new ComponentController();
+            compControl.setMainFrame(newApplicatioFrame);
+            compControl.setVisualisationComponent(newApplicatioFrame.getVisualisationComponent());
+            compControl.setConfiguration(configuration);
+            newApplicatioFrame.setController(compControl);
+        } catch (Exception ex) {
+            if (logger.isErrorEnabled()) {
+                final String message = "Error during initialisation of ComponentController.";
+                logger.error(message, ex);
+                throw new InitialisationException(message, ex);
+            }
+        }
+        return newApplicatioFrame;
+    }
+
+    public static JPanel getVisualisationPanel(final Properties configurationProperties) {
+        //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: implemenent SingleMode
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
 }
