da3b203274e040d79f520c7a5687625d5f5baafa
Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>
2011-02-08 09:42:28 +0100

    Basic implementation of coordinate transformation.
    	* Hardcoded usage of coordinate transformation. There must be two optimisations
    	  in the next version. In the grids only the origin and maybe the cellsize must
    	  be transformated. Also the source coordinate system should be detected by the
    	  application and then the appropriate transformation should be carried out.
    
    Signed-off-by: Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>

--- src/main/java/com/dfki/av/sudplan/io/dem/ArcGridParser.java
+++ src/main/java/com/dfki/av/sudplan/io/dem/ArcGridParser.java
@@ -1,11 +1,11 @@
 package com.dfki.av.sudplan.io.dem;
 
+import com.dfki.av.sudplan.util.CoordinateTransformer;
+import com.dfki.av.sudplan.control.ComponentBroker;
 import com.dfki.av.sudplan.io.FileFormatException;
 import com.dfki.av.sudplan.io.ParsingException;
 import com.dfki.av.sudplan.io.SplitNotPossibleException;
-import com.dfki.av.sudplan.util.Measurement;
 import com.dfki.av.sudplan.util.TimeMeasurement;
-import java.util.Arrays;
 import java.io.BufferedReader;
 
 import javax.vecmath.Point3f;
@@ -26,6 +26,9 @@ public class ArcGridParser {
 
     //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: documentation/unit tests
     //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: possible to parse without ncols/nrows (no array)
+    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: if there are more parsers make infrastructure for scaling,transformation etc.
+    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: performance improvement.
+    ////ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: for grid reprojection it should be sufficient to transform the orgin.
     private final static Logger logger = LoggerFactory.getLogger(ArcGridParser.class);
     private RawArcGrid arcGrid = new RawArcGrid();
     private Point3f[] pointList;
@@ -48,6 +51,7 @@ public class ArcGridParser {
             logger.debug("Parsing ArcInfo Grid.");
         }
         try {
+            arcGrid.setScaleFactor((float) ComponentBroker.getInstance().getScalingFactor());
             BufferedReader sr = new BufferedReader(reader);
             // READ PARAMETERFILES OF Arcinfo Grid
             //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>:problem this must be variable --> no_data need not be there
@@ -68,16 +72,16 @@ public class ArcGridParser {
                 logger.debug("Number of rows= " + arcGrid.getNumberOfRows());
             }
             currentKeyValuePair = doubleSplit(sr.readLine());
-            if (!currentKeyValuePair[0].equalsIgnoreCase(LEFTMOST_X_COORDINATE)) {
-                throw new FileFormatException("No x orgin value specified(" + LEFTMOST_X_COORDINATE + ")");
+            if (!currentKeyValuePair[0].equalsIgnoreCase(X_MINIMUM)) {
+                throw new FileFormatException("No x orgin value specified(" + X_MINIMUM + ")");
             }
-            arcGrid.setLeftmostXValue(Float.parseFloat(currentKeyValuePair[1]));
+            arcGrid.setXMin(Float.parseFloat(currentKeyValuePair[1]));
 
             currentKeyValuePair = doubleSplit(sr.readLine());
-            if (!currentKeyValuePair[0].equalsIgnoreCase(BOTTOMMOST_Y_COORDINATE)) {
-                throw new FileFormatException("No y orgin value specified(" + BOTTOMMOST_Y_COORDINATE + ")");
+            if (!currentKeyValuePair[0].equalsIgnoreCase(Y_MINIMUM)) {
+                throw new FileFormatException("No y orgin value specified(" + Y_MINIMUM + ")");
             }
-            arcGrid.setBottommostYValue(Float.parseFloat(currentKeyValuePair[1]));
+            arcGrid.setYMin(Float.parseFloat(currentKeyValuePair[1]));
 
             logger.debug("Grid origin= {}", arcGrid.getOrigin());
             currentKeyValuePair = doubleSplit(sr.readLine());
@@ -103,6 +107,8 @@ public class ArcGridParser {
                     logger.info("No data value is not avaialable");
                 }
             }
+            //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>:transform orgin;
+            //            CoordinateTransformer.transformPointToInternalCoordinateSystem(2400,sourcePoint);
 //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: make this configurable in order to only read a part of the grid.
 //      arcGrid.setNumberOfRows(100);
 //      arcGrid.setNumberOfColumns(100);
@@ -131,10 +137,36 @@ public class ArcGridParser {
                     z = Float.parseFloat(currentRow[currentColumn]);
                     //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>:evil hack only for looking at the moment
                     if (z == arcGrid.getNoDataValue()) {
-                        z = 1;
+                        z = 0.0f;
                     }
-                    pointList[coordinateCount] = new Point3f((x) * (new Float(arcGrid.getCellsize()) * arcGrid.getScaleFactor()), (y)
-                            * (new Float(arcGrid.getCellsize()) * arcGrid.getScaleFactor()), z * arcGrid.getScaleFactor() * arcGrid.getzExaggeration());
+                    final float sourceX = (arcGrid.getXMin() + (x * (new Float(arcGrid.getCellsize()))));
+                    final float sourceY = (arcGrid.getYMax() + (y * (new Float(arcGrid.getCellsize()))));
+                    final float sourceZ = z * arcGrid.getScaleFactor() * arcGrid.getzExaggeration();
+                    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: the transformation is pretty expensive would be more effiecient to parse all at once check!
+                    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: maybe there is a much simpler transformation from 4124 to 4326 --> e.g. by simply taking the values as lat/long the difference is only in the decimal check!
+                    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: determine epsg from file
+//                    final Point3f transformedPoint = CoordinateTransformer.transformPointToInternalCoordinateSystem(4326, new Point3f(sourceX, sourceY, sourceZ));
+                    final Point3f sourcePoint = new Point3f(sourceX, sourceY, sourceZ);
+//                    if (logger.isDebugEnabled()) {
+//                        logger.debug("source: " + sourcePoint);
+//                    }
+//                    final Point3f transformedPoint = CoordinateTransformer.transformPointToInternalCoordinateSystem(4326,sourcePoint);
+                    final Point3f transformedPoint = sourcePoint;
+//                    if (logger.isDebugEnabled()) {
+//                        logger.debug("point3f: " + transformedPoint);
+//                    }
+                    transformedPoint.x *= arcGrid.getScaleFactor();
+                    transformedPoint.y *= arcGrid.getScaleFactor();
+//                    if (logger.isDebugEnabled()) {
+//                        logger.debug("scaled: " + transformedPoint);
+//                    }
+                    pointList[coordinateCount] = transformedPoint;
+//                    if (logger.isDebugEnabled()) {
+////                        logger.debug("x: "+x+" cell: "+arcGrid.getCellsize()+" scale: "+arcGrid.getScaleFactor());
+////                        logger.debug("calculated x: "+(((x) * (new Float(arcGrid.getCellsize()))) )* arcGrid.getScaleFactor());
+////                        logger.debug("point3f x:"+pointList[coordinateCount].getX()+" y:"+pointList[coordinateCount].getY());
+//                        logger.debug("point3f: "+pointList[coordinateCount]);
+//                    }
                     x++;
                     if (x == arcGrid.getNumberOfColumns()) {
                         x = 0;
@@ -145,7 +177,8 @@ public class ArcGridParser {
                 rowCount++;
             }
             if (logger.isDebugEnabled()) {
-                logger.debug("Coordinates successfully parsed.  Time elapsed: " + TimeMeasurement.getInstance().stopMeasurement(this).getDuration() + " ms");
+                logger.debug("Coordinates successfully parsed.  Time elapsed: "
+                        + TimeMeasurement.getInstance().stopMeasurement(this).getDuration() + " ms");
             }
             sr.close();
         } catch (Exception ex) {
@@ -156,11 +189,15 @@ public class ArcGridParser {
         }
         arcGrid.setRawCoordinates(pointList);
         if (logger.isDebugEnabled()) {
-            logger.debug("Grid dimensions: coordinate count: " + arcGrid.getCoordinateCount() + ", number of rows: " + arcGrid.getNumberOfRows() + ", number of columns: " + arcGrid.getNumberOfColumns());
+            logger.debug("Grid dimensions: coordinate count: " + arcGrid.getCoordinateCount()
+                    + ", number of rows: " + arcGrid.getNumberOfRows()
+                    + ", number of columns: " + arcGrid.getNumberOfColumns());
         }
         arcGrid.setSparseFactor(1.0f);
         if (logger.isDebugEnabled()) {
-            logger.debug("Grid dimensions: coordinate count: " + arcGrid.getCoordinateCount() + ", number of rows: " + arcGrid.getNumberOfRows() + ", number of columns: " + arcGrid.getNumberOfColumns());
+            logger.debug("Grid dimensions: coordinate count: " + arcGrid.getCoordinateCount()
+                    + ", number of rows: " + arcGrid.getNumberOfRows()
+                    + ", number of columns: " + arcGrid.getNumberOfColumns());
         }
         return arcGrid;
     }
