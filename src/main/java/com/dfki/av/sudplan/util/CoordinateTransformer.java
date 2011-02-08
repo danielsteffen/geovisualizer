fbf4f97067f1b67e4a95eab42983881f12319f13
Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>
2011-02-08 09:30:33 +0100

    First implementation of a coordinate transformation class.
    	* This class represents a first very basic implementation of a class
    	  which is able to transform coordinates from one reference system/projection
    	  to another (e.g. epsg:2400 (Sweden) to epsg:4326 (WGS84). The class also
    	  transforms the coordinates further to the internal projection Plate Carree.
    	  Currently there are two implementations (Geotools,Deegree), the experience so
    	  far are that geotools does not calculate the coordinates accuratley, deegree on
    	  the other hand seem to calculate the coordinates correctly.
    
    Signed-off-by: Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>

---
+++ src/main/java/com/dfki/av/sudplan/util/CoordinateTransformer.java
@@ -0,0 +1,123 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dfki.av.sudplan.util;
+
+import javax.vecmath.Point3f;
+//import org.deegree.cs.CRS;
+//import org.deegree.cs.exceptions.TransformationException;
+//import org.deegree.cs.exceptions.UnknownCRSException;
+//import org.deegree.geometry.GeometryTransformer;
+//import org.deegree.geometry.standard.primitive.DefaultPoint;
+import org.deegree.cs.CRS;
+import org.deegree.geometry.GeometryTransformer;
+import org.deegree.geometry.standard.primitive.DefaultPoint;
+//import org.geotools.referencing.CRS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Sebastian Puhl <sebastian.puhl@dfki.de>
+ * @version 1.0
+ * @since 1.6
+ */
+public class CoordinateTransformer {
+
+    public static final int WGS84_EPGS = 4326;
+    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: does not work with geotools the resulting coordinates are deegree/rads
+//    public static final int PLATE_CARREE_EPGS = 32662;
+    //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>:bad design investigate further must be a way not to do the conversion by hand
+    //this is wrong by the idea
+    public static final int PLATE_CARREE_EPGS = 4326;
+    public static final int INTERNAL_EPSG = PLATE_CARREE_EPGS;
+    private static final Logger logger = LoggerFactory.getLogger(CoordinateTransformer.class);
+
+// GeoTools problem with 4124
+    public static Point3f transformPoint(final CRS source, final CRS target, final Point3f point) throws
+            TransformationException {
+        if (point == null) {
+            throw new IllegalArgumentException("Transformation not possible point is null.");
+        }
+        try {
+            final Point3f transformedPoint = new Point3f();
+            final GeometryTransformer transformer = new GeometryTransformer(target);
+            final DefaultPoint sourcePoint = new DefaultPoint(
+                    null, source, null, new double[]{point.x, point.y, point.z});
+            DefaultPoint deegreeTransformedPoint =
+                    (DefaultPoint) transformer.transform(sourcePoint, source.getWrappedCRS());
+            transformedPoint.x = (float) deegreeTransformedPoint.get0();
+            transformedPoint.y = (float) deegreeTransformedPoint.get1();
+            return transformedPoint;
+        } catch (Exception ex) {
+            final String message = "Error while transforming point.";
+            if (logger.isErrorEnabled()) {
+                logger.error(message, ex);
+            }
+            throw new TransformationException(message, ex);
+        }
+    }
+
+    public static Point3f transformPoint(final int source, final int target, final Point3f point) throws
+            TransformationException {
+        final CRS sourceCRS = new CRS("epsg:" + source);
+        final CRS targetCRS = new CRS("epsg:" + target);
+        return transformPoint(sourceCRS, targetCRS, point);
+    }
+//    public static Point3f transformPoint(CoordinateReferenceSystem source, CoordinateReferenceSystem target, Point3f point) throws TransformationException {
+//        if (point == null) {
+//            throw new IllegalArgumentException("Transformation not possible point is null.");
+//        }
+//        try {
+//            final Point3f transformedPoint = new Point3f();
+//            MathTransform transformation = CRS.findMathTransform(source, target);
+//            final float[] transformedCoordinates = new float[3];
+//            point.get(transformedCoordinates);
+//            transformation.transform(transformedCoordinates, 0, transformedCoordinates, 0, 1);
+//            transformedPoint.set(transformedCoordinates);
+//            return transformedPoint;
+//        } catch (Exception ex) {
+//            final String message = "Error while transforming point.";
+//            if (logger.isErrorEnabled()) {
+//                logger.error(message, ex);
+//            }
+//            throw new TransformationException(message, ex);
+//        }
+//    }
+//    public static Point3f transformPoint(int source, int target, Point3f point) throws TransformationException {
+//
+//        try {
+//            final CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:" + source);
+//            final CoordinateReferenceSystem targetCRS = CRS.decode("EPSG:" + target);
+//            return transformPoint(sourceCRS, targetCRS, point);
+//        } catch (Exception ex) {
+//            final String message = "Error while transforming point.";
+//            if (logger.isErrorEnabled()) {
+//                logger.error(message, ex);
+//            }
+//            throw new TransformationException(message, ex);
+//        }
+//
+//
+//    }
+
+    public static Point3f transformPointToInternalCoordinateSystem(final int source, final Point3f point)
+            throws TransformationException {
+//        if (logger.isDebugEnabled()) {
+//            logger.debug("source point: "+point);
+//        }
+        final Point3f transformedPoint = transformPoint(source, INTERNAL_EPSG, point);
+        //ToDo Sebastian Puhl <sebastian.puhl@dfki.de>: see above don't want to do this by hand. Must be a good way
+//        if (logger.isDebugEnabled()) {
+//            logger.debug("transformed point: "+transformedPoint);
+//        }
+        if (INTERNAL_EPSG == PLATE_CARREE_EPGS) {
+            transformedPoint.x =
+                    (float) (EarthFlat.deegreeToRadians(transformedPoint.x) * EarthFlat.WGS84_EARTH_EQUATORIAL_RADIUS);
+            transformedPoint.y =
+                    (float) (EarthFlat.deegreeToRadians(transformedPoint.y) * EarthFlat.WGS84_EARTH_EQUATORIAL_RADIUS);
+        }
+        return transformedPoint;
+    }
+}
