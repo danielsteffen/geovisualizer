bea45a8af2367008c311ecd2d9b51bb4073441e9
Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>
2011-02-17 12:15:34 +0100

    New examples starter
    	* The class is no longer valid after the refactoring of the examples.
    
    Signed-off-by: Sebastian Puhl <sebastian.puhl@dfki.uni-kl.de>

--- src/main/java/com/dfki/av/sudplan/example/ExampleStarter.java
+++ src/main/java/com/dfki/av/sudplan/example/ExampleStarter.java
@@ -21,7 +21,7 @@ public class ExampleStarter {
           frame.setSize(800,600);
           frame.setLayout(new BorderLayout());
           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          frame.add(new Example3().createUniverse(),BorderLayout.CENTER);
+          frame.add(new TriangleArray().createUniverse(),BorderLayout.CENTER);
           frame.setVisible(true);
       }
 }
