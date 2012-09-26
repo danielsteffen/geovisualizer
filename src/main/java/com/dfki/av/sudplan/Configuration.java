ca5efb21a1f37e048b6ac65717a5acc173d58fe3
Daniel Steffen <daniel.steffen@dfki.de>
2012-09-26 14:34:43 +0200

    Enhanced configuration to validate for version conflict.
    * Enhanced configuration to validate for version conflict. Checking for
      <version> tag as well as the value of the tag in sudplan3D.xml.
    * Set sudplan3D initial version to 1.1.
    
    Signed-off-by: Daniel Steffen <daniel.steffen@dfki.de>

--- src/main/java/com/dfki/av/sudplan/Configuration.java
+++ src/main/java/com/dfki/av/sudplan/Configuration.java
@@ -20,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * The class responsible for the configuration of the sudplan3D application.
  *
  * @author Daniel Steffen <daniel.steffen at dfki.de>
  */
@@ -33,6 +34,10 @@ public final class Configuration {
      * The sudplan3D image used as window and dialog icons.
      */
     public static final Image SUDPLAN_3D_IMAGE = initImage();
+    /**
+     * The current version of the configuration file.
+     */
+    private static final String VERSION = "1.1";
     /**
      * The sudplan3D configuration.
      */
@@ -58,33 +63,32 @@ public final class Configuration {
     }
 
     /**
-     * Initialize the
+     * Initialize the {@link XMLConfiguration} for the application.
+     *
+     * @return the {@link #XML_CONFIG} to return.
      */
     private static XMLConfiguration initConfig() {
         log.debug("Init sudplan3D user home...");
         String userHomeDir = VisSettings.USER_HOME_DIR;
 
-        log.info("Init sudplan3D working directory...");
-        File workingDir = new File(userHomeDir);
-
         log.info("Init sudplan3D configuration...");
         // Check whether the sudplan3D config file already exists.
-        File userConfigFile = new File(userHomeDir + "/config/sudplan3D.xml");
+        final File userConfigFile = new File(userHomeDir + "/config/sudplan3D.xml");
         if (!userConfigFile.exists()) {
-            log.debug("Setting up {}...", userConfigFile.getAbsoluteFile());
-            try {
-                ClassLoader loader = Configuration.class.getClassLoader();
-                URL url = loader.getResource("config/sudplan3D.xml");
-                XMLConfiguration xmlInitialConfig = new XMLConfiguration(url);
-                xmlInitialConfig.addProperty("sudplan3D.user.dir", userHomeDir);
-                xmlInitialConfig.addProperty("sudplan3D.working.dir", workingDir.getAbsolutePath());
-                xmlInitialConfig.save(userConfigFile);
-            } catch (ConfigurationException ex) {
-                log.error(ex.toString());
+            log.debug("No configuration file existing.");
+            installConfigFile(userConfigFile, userHomeDir, userHomeDir);
+        } else {
+            log.debug("Configuration file existing. Checking version ...");
+            if (!isConfigFileSupported(userConfigFile)) {
+                log.debug("Configuration file not supported.");
+                log.debug("Delete old configuration file.");
+                userConfigFile.delete();
+                installConfigFile(userConfigFile, userHomeDir, userHomeDir);
+            } else {
+                log.debug("Configuration file supported.");
             }
         }
 
-        log.debug("{} exists.", userConfigFile.getAbsoluteFile());
         XMLConfiguration xmlConfig = null;
         try {
             xmlConfig = new XMLConfiguration(userConfigFile);
@@ -97,6 +101,58 @@ public final class Configuration {
             log.error(ex.toString());
         }
         return xmlConfig;
+    }
+
+    /**
+     * Save the configuration file {@code config/sudplan3D.xml} from the
+     * resources to the the file {@code file}. Adding the additional properties
+     * {@code sudplan3D.user.dir} and {@code sudplan3D.working.dir}.
+     *
+     * @param file the configuration {@link File}
+     * @param userHomeDir the user home directory
+     * @param workingDir the working directory
+     */
+    private static void installConfigFile(File file, String userHomeDir, String workingDir) {
+        log.debug("Installing configuration to {}...", file.getAbsoluteFile());
+        try {
+            ClassLoader loader = Configuration.class.getClassLoader();
+            URL url = loader.getResource("config/sudplan3D.xml");
+            XMLConfiguration xmlInitialConfig = new XMLConfiguration(url);
+            xmlInitialConfig.addProperty("sudplan3D.user.dir", userHomeDir);
+            xmlInitialConfig.addProperty("sudplan3D.working.dir", workingDir);
+            xmlInitialConfig.save(file);
+        } catch (ConfigurationException ex) {
+            log.error(ex.toString());
+        }
+    }
+
+    /**
+     * Returns whether the configuration file {@code file} is supported by the
+     * current implementation.
+     *
+     * @param file the configuration {@link File} to check
+     * @return {@code true} if a {@code version} tag exists and its value is
+     * equal to {@link #VERSION}. Otherwise {@code false}.
+     */
+    private static boolean isConfigFileSupported(File file) {
+        try {
+            XMLConfiguration xmlConfig = new XMLConfiguration(file);
+            if (!xmlConfig.containsKey("version")) {
+                log.debug("No version tag.");
+                return false;
+            }
+
+            String version = xmlConfig.getString("version");
+            if (version.equalsIgnoreCase(VERSION)) {
+                return true;
+            } else {
+                log.debug("Version {} not supported.", version);
+                return false;
+            }
+        } catch (ConfigurationException ex) {
+            log.error(ex.toString());
+        }
+        return false;
     }
 
     /**
